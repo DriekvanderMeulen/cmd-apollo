748b9dba26d1f13ed557786d79f1740b239f0bdf
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-09-24 12:58:30 +0200

    DEMO TIME AAAYOOO

--- apps/app/app/(tabs)/index.tsx
+++ apps/app/app/(tabs)/index.tsx
@@ -1,41 +1,46 @@
-import { useEffect } from 'react'
 import { ThemedView } from '@/components/themed-view'
 import { ThemedText } from '@/components/themed-text'
 import { CameraView, useCameraPermissions } from 'expo-camera'
-import { Pressable } from 'react-native'
+import { Pressable, Linking } from 'react-native'
 
 export default function HomeScreen() {
 	const [permission, requestPermission] = useCameraPermissions()
 
-	useEffect(function requestOnLoad() {
-		if (!permission) return
-		if (!permission.granted && !permission.canAskAgain) return
-		if (!permission.granted) requestPermission()
-	}, [permission, requestPermission])
+	const isGranted = permission?.granted ?? false
+	const canAskAgain = permission?.canAskAgain ?? true
 
-	if (!permission) {
-		return <ThemedView className="flex-1 bg-black" />
-	}
-
-	if (!permission.granted) {
+	if (!isGranted) {
 		return (
 			<ThemedView className="flex-1 items-center justify-center px-6">
 				<ThemedText className="mb-4 text-center">
 					We need your permission to use the camera
 				</ThemedText>
-				<Pressable
-					onPress={requestPermission}
-					className="rounded-md bg-black px-4 py-2 dark:bg-white"
-				>
-					<ThemedText>Grant permission</ThemedText>
-				</Pressable>
+				{canAskAgain ? (
+					<Pressable
+						onPress={requestPermission}
+						className="rounded-md px-4 py-2 bg-neutral-900 dark:bg-white"
+					>
+						<ThemedText className="text-white dark:text-black">
+							Grant permission
+						</ThemedText>
+					</Pressable>
+				) : (
+					<Pressable
+						onPress={Linking.openSettings}
+						className="rounded-md px-4 py-2 bg-neutral-900 dark:bg-white"
+					>
+						<ThemedText className="text-white dark:text-black">
+							Open settings
+						</ThemedText>
+					</Pressable>
+				)}
 			</ThemedView>
 		)
 	}
 
 	return (
-		<ThemedView className="flex-1 bg-black">
-			<CameraView className="flex-1" active={true} />
+		<ThemedView className="flex-1">
+			<CameraView style={{ flex: 1 }} active={true} facing="back" />
 		</ThemedView>
 	)
 }
