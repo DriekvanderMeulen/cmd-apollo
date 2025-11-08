41cebbcfe3f9a6fd337b62887844ec45a0176930
Driek van der Meulen <driekvandermeulen@icloud.com>
2025-11-08 14:07:12 +0100

    Update app configuration to include CMS URL and read token
    - Enhanced app.config.ts to merge existing extra configuration with new CMS URL and read token from environment variables.
    - Updated app.json to include additional associated domains and improved structure for better readability and maintainability.

--- apps/app/app.config.ts
+++ apps/app/app.config.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 export default ({ config }: { config: any }) => ({
 	...config,
 	extra: {
+		...config.extra,
 		cmsUrl: process.env.EXPO_PUBLIC_CMS_URL,
 		cmsReadToken: process.env.EXPO_PUBLIC_CMS_READ_TOKEN,
 	},
